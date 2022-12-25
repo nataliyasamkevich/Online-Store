cfa634d6808acf5f720d0a46c3329d5d7c799021
Lizaveta <lizadondysh@icloud.com>
2022-12-25 02:24:02 +0200

    feat: adjust configs for the current project

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
+const isProduction = process.env.NODE_ENV == 'production';
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV == 'production';
-
-
 const stylesHandler = 'style-loader';
-
-
 
 const config = {
     entry: './src/index.ts',
@@ -44,8 +41,18 @@ const config = {
                 use: [stylesHandler, 'css-loader', 'postcss-loader'],
             },
             {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
+                test: /\.(ttf|woff|woff2|eot)$/i,
+                type: 'asset/resource',
+                generator: {
+                  filename: 'assets/fonts/[name][ext]'
+                }
+            },
+            {
+                test: /\.(jpe?g|png|webp|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[ext]/[name][ext]'
+                }
             },
 
             // Add your rules for custom modules here
@@ -60,10 +67,7 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
-        
         config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-        
     } else {
         config.mode = 'development';
     }
