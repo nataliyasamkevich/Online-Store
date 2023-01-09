2935de38d5b3949be4f661b63f851a5af52b7eff
Lizaveta <lizadondysh@icloud.com>
2023-01-09 23:23:58 +0200

    fix: remove unnecessary special characters from regex

--- src/controllers/modal.ts
+++ src/controllers/modal.ts
@@ -1,6 +1,10 @@
 import { Validatable } from '../base/base';
 
-function autoBind(target: Object, name: string, descriptor: PropertyDescriptor) {
+function autoBind(
+  target: object,
+  name: string,
+  descriptor: PropertyDescriptor
+) {
   const originalMethod = descriptor.value;
 
   const newDescriptor: PropertyDescriptor = {
@@ -8,7 +12,7 @@ function autoBind(target: Object, name: string, descriptor: PropertyDescriptor) 
     get() {
       return originalMethod.bind(this);
     },
-  }
+  };
 
   return newDescriptor;
 }
@@ -22,16 +26,27 @@ function validate(validatableInput: Validatable): boolean {
 
   if (validatableInput.minLengthCharacters != null) {
     const minLengthCharactersValue = validatableInput.minLengthCharacters;
-    isValid = isValid
-      && validatableInput.value.split(' ').filter(value => value !== '').every(value => value.length >= minLengthCharactersValue);
+    isValid =
+      isValid &&
+      validatableInput.value
+        .split(' ')
+        .filter((value) => value !== '')
+        .every((value) => value.length >= minLengthCharactersValue);
   }
 
   if (validatableInput.countWord != null) {
-    isValid = isValid && validatableInput.value.split(' ').map(value => value.trim()).filter(value => value !== '').length >= validatableInput.countWord;
+    isValid =
+      isValid &&
+      validatableInput.value
+        .split(' ')
+        .map((value) => value.trim())
+        .filter((value) => value !== '').length >= validatableInput.countWord;
   }
 
   if (validatableInput.minLength != null) {
-    isValid = isValid && validatableInput.value.trim().length >= validatableInput.minLength;
+    isValid =
+      isValid &&
+      validatableInput.value.trim().length >= validatableInput.minLength;
   }
 
   if (validatableInput.regexp != null) {
@@ -41,12 +56,18 @@ function validate(validatableInput: Validatable): boolean {
 
   if (validatableInput.maxValueMonth != null) {
     if (validatableInput)
-      isValid = isValid && Number(validatableInput.value.slice(0, 2)) <= validatableInput.maxValueMonth;
+      isValid =
+        isValid &&
+        Number(validatableInput.value.slice(0, 2)) <=
+          validatableInput.maxValueMonth;
   }
 
   if (validatableInput.minValueYear != null) {
     if (validatableInput)
-      isValid = isValid && Number(validatableInput.value.slice(3, 5)) >= validatableInput.minValueYear;
+      isValid =
+        isValid &&
+        Number(validatableInput.value.slice(3, 5)) >=
+          validatableInput.minValueYear;
   }
 
   return isValid;
@@ -69,15 +90,25 @@ export class ValidateProjectInput {
   constructor() {
     this.formEl = <HTMLFormElement>document.querySelector('.form');
     this.nameEl = <HTMLInputElement>document.getElementById('name');
-    this.phoneNumberEl = <HTMLInputElement>document.getElementById('phoneNumber');
+    this.phoneNumberEl = <HTMLInputElement>(
+      document.getElementById('phoneNumber')
+    );
     this.emailEl = <HTMLInputElement>document.getElementById('e-mail');
     this.addressEl = <HTMLTextAreaElement>document.getElementById('address');
-    this.cardNumberEl = <HTMLInputElement>document.getElementById('card-number');
-    this.expirationDateEl = <HTMLInputElement>document.getElementById('expiration-date');
+    this.cardNumberEl = <HTMLInputElement>(
+      document.getElementById('card-number')
+    );
+    this.expirationDateEl = <HTMLInputElement>(
+      document.getElementById('expiration-date')
+    );
     this.cvvEl = <HTMLInputElement>document.getElementById('cvv');
     this.cardLogo = <HTMLImageElement>document.querySelector('.card__logo');
-    this.errorMessage = <HTMLDivElement>document.querySelector('.error-message');
-    this.formGroupClose = <HTMLDivElement>document.querySelector('.form-group__close');
+    this.errorMessage = <HTMLDivElement>(
+      document.querySelector('.error-message')
+    );
+    this.formGroupClose = <HTMLDivElement>(
+      document.querySelector('.form-group__close')
+    );
     this.modal = <HTMLDivElement>document.querySelector('.modal');
 
     this.configure();
@@ -94,19 +125,37 @@ export class ValidateProjectInput {
 
   private validateCardNumberField(): void {
     this.cardNumberEl.addEventListener('input', (event: InputEventInit) => {
-      if (!/[0-9]/.test(this.cardNumberEl.value) || this.cardNumberEl.value.length > 19) {
-        this.cardNumberEl.value = this.cardNumberEl.value.slice(0, this.cardNumberEl.value.length - 1)
+      if (
+        !/[0-9]/.test(this.cardNumberEl.value) ||
+        this.cardNumberEl.value.length > 19
+      ) {
+        this.cardNumberEl.value = this.cardNumberEl.value.slice(
+          0,
+          this.cardNumberEl.value.length - 1
+        );
       }
 
-      if (this.cardNumberEl.value.match(/[^\0-9\-]/)) {
-        this.cardNumberEl.value = this.cardNumberEl.value.replace(/[^\0-9\-]/, '');
-      };
-
-      if (event.inputType === 'deleteContentBackward' && /[0-9]{4}/.test(this.cardNumberEl.value.slice(-4))) {
-        this.cardNumberEl.value = this.cardNumberEl.value.slice(0, this.cardNumberEl.value.length - 1)
+      if (this.cardNumberEl.value.match(/[^\0-9-]/)) {
+        this.cardNumberEl.value = this.cardNumberEl.value.replace(
+          /[^\0-9-]/,
+          ''
+        );
       }
 
-      if (/[0-9]{4}/.test(this.cardNumberEl.value.slice(-4)) && this.cardNumberEl.value.length < 19) {
+      if (
+        event.inputType === 'deleteContentBackward' &&
+        /[0-9]{4}/.test(this.cardNumberEl.value.slice(-4))
+      ) {
+        this.cardNumberEl.value = this.cardNumberEl.value.slice(
+          0,
+          this.cardNumberEl.value.length - 1
+        );
+      }
+
+      if (
+        /[0-9]{4}/.test(this.cardNumberEl.value.slice(-4)) &&
+        this.cardNumberEl.value.length < 19
+      ) {
         this.cardNumberEl.value += ' ';
       }
     });
@@ -114,19 +163,37 @@ export class ValidateProjectInput {
 
   private validateExpirationDateField(): void {
     this.expirationDateEl.addEventListener('input', (event: InputEventInit) => {
-      if (!/[0-9]/.test(this.expirationDateEl.value) || this.expirationDateEl.value.length > 5) {
-        this.expirationDateEl.value = this.expirationDateEl.value.slice(0, this.expirationDateEl.value.length - 1)
+      if (
+        !/[0-9]/.test(this.expirationDateEl.value) ||
+        this.expirationDateEl.value.length > 5
+      ) {
+        this.expirationDateEl.value = this.expirationDateEl.value.slice(
+          0,
+          this.expirationDateEl.value.length - 1
+        );
       }
 
-      if (this.expirationDateEl.value.match(/[^0-9\/]/g)) {
-        this.expirationDateEl.value = this.expirationDateEl.value.replace(/[^0-9\/]/g, '');
-      };
-
-      if (event.inputType === 'deleteContentBackward' && /[0-9]{2}/.test(this.expirationDateEl.value.slice(-2))) {
-        this.expirationDateEl.value = this.expirationDateEl.value.slice(0, this.expirationDateEl.value.length - 1)
+      if (this.expirationDateEl.value.match(/[^0-9/]/g)) {
+        this.expirationDateEl.value = this.expirationDateEl.value.replace(
+          /[^0-9/]/g,
+          ''
+        );
       }
 
-      if (/[0-9]{2}/.test(this.expirationDateEl.value.slice(-2)) && this.expirationDateEl.value.length < 4) {
+      if (
+        event.inputType === 'deleteContentBackward' &&
+        /[0-9]{2}/.test(this.expirationDateEl.value.slice(-2))
+      ) {
+        this.expirationDateEl.value = this.expirationDateEl.value.slice(
+          0,
+          this.expirationDateEl.value.length - 1
+        );
+      }
+
+      if (
+        /[0-9]{2}/.test(this.expirationDateEl.value.slice(-2)) &&
+        this.expirationDateEl.value.length < 4
+      ) {
         this.expirationDateEl.value += '/';
       }
     });
@@ -136,11 +203,11 @@ export class ValidateProjectInput {
     this.cvvEl.addEventListener('input', () => {
       if (this.cvvEl.value.length > 3) {
         this.cvvEl.value = this.cvvEl.value.slice(0, 3);
-      };
+      }
 
       if (this.cvvEl.value.match(/[^0-9]/g)) {
         this.cvvEl.value = this.cvvEl.value.replace(/[^0-9]/g, '');
-      };
+      }
     });
   }
 
@@ -148,19 +215,19 @@ export class ValidateProjectInput {
     this.cardNumberEl.addEventListener('input', () => {
       const cardNumber = this.cardNumberEl.value;
       if (cardNumber[0] == '2') {
-        this.cardLogo.src = './../../assets/img/payment-sys-mir.png'
+        this.cardLogo.src = './../../assets/img/payment-sys-mir.png';
       } else if (cardNumber[0] == '3') {
-        this.cardLogo.src = './../../assets/img/payment-sys-ae.png'
+        this.cardLogo.src = './../../assets/img/payment-sys-ae.png';
       } else if (cardNumber[0] == '4') {
-        this.cardLogo.src = './../../assets/img/payment-sys-visa.png'
+        this.cardLogo.src = './../../assets/img/payment-sys-visa.png';
       } else if (cardNumber[0] == '5') {
-        this.cardLogo.src = './../../assets/img/payment-sys-mastercard.png'
+        this.cardLogo.src = './../../assets/img/payment-sys-mastercard.png';
       } else if (cardNumber[0] == '6') {
-        this.cardLogo.src = './../../assets/img/payment-sys-belcard.png'
+        this.cardLogo.src = './../../assets/img/payment-sys-belcard.png';
       } else if (cardNumber[0] == null) {
         this.cardLogo.src = './../../assets/img/payment-sys-nologo.png';
       }
-    })
+    });
   }
 
   @autoBind
@@ -176,7 +243,8 @@ export class ValidateProjectInput {
 
   private submitUndCloseModal(): void {
     setTimeout(() => this.modal.classList.add('modal__hidden'), 5000);
-    this.formEl.innerHTML = 'Thanks for your order. Redirect to the store after 5 sec'
+    this.formEl.innerHTML =
+      'Thanks for your order. Redirect to the store after 5 sec';
   }
 
   private gatherUserInput(): string[] | void {
@@ -193,19 +261,19 @@ export class ValidateProjectInput {
       required: true,
       minLengthCharacters: 3,
       countWord: 2,
-    }
+    };
 
     const phoneNumberValidatable: Validatable = {
       value: phoneNumber,
       required: true,
-      regexp: /^[\+][\d\(\)\ -]{8,}\d$/,
-    }
+      regexp: /^[+][\d() -]{8,}\d$/,
+    };
 
     const emailValidatable: Validatable = {
       value: email,
       required: true,
-      regexp: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-    }
+      regexp: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+    };
 
     const addressValidatable: Validatable = {
       value: address,
@@ -213,14 +281,14 @@ export class ValidateProjectInput {
       minLengthCharacters: 5,
       countWord: 3,
       regexp: /^([0-9a-zA-Z]{5,} ){2}[0-9a-zA-Z]{5,}/,
-    }
+    };
 
     const cardNumberValidatable: Validatable = {
       value: cardNumber,
       required: true,
       minLength: 19,
       regexp: /^([0-9]{4} ){3}[0-9]{4}$/,
-    }
+    };
 
     const expirationDateValidatable: Validatable = {
       value: expirationDate,
@@ -229,17 +297,23 @@ export class ValidateProjectInput {
       regexp: /^\d{2}\/\d{2}$/,
       maxValueMonth: 12,
       minValueYear: 23,
-    }
+    };
 
     const cvvValidatable: Validatable = {
       value: cvv,
       required: true,
       minLength: 3,
-    }
+    };
 
-    if (!validate(nameValidatable) || !validate(phoneNumberValidatable) || !validate(emailValidatable)
-      || !validate(addressValidatable) || !validate(cardNumberValidatable) || !validate(expirationDateValidatable)
-      || !validate(cvvValidatable)) {
+    if (
+      !validate(nameValidatable) ||
+      !validate(phoneNumberValidatable) ||
+      !validate(emailValidatable) ||
+      !validate(addressValidatable) ||
+      !validate(cardNumberValidatable) ||
+      !validate(expirationDateValidatable) ||
+      !validate(cvvValidatable)
+    ) {
       this.showErrorMessage(nameValidatable, this.nameEl);
       this.showErrorMessage(phoneNumberValidatable, this.phoneNumberEl);
       this.showErrorMessage(emailValidatable, this.emailEl);
